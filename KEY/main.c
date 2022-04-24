1d84ecd804d864b38059b73f3930d99152d81125
bin <bin@fpga.net.cn>
2022-04-24 14:06:02 +0800

    增加按键

---
+++ KEY/main.c
@@ -0,0 +1,179 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      新核科技(广州)有限公司
+//
+//      Copyright (C) 2022 CoreKernel Technology Guangzhou Co., Ltd
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      GPIO KEY 按键
+//
+//      2022年04月24日
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/*
+ *    按键中断
+ *
+ *    - 希望缄默(bin wang)
+ *    - bin@corekernel.net
+ *
+ *    官网 corekernel.net/.org/.cn
+ *    社区 fpga.net.cn
+ *
+ */
+#include "hw_types.h"
+#include "hw_syscfg0_C6748.h"
+
+#include "soc_C6748.h"
+
+#include "psc.h"
+#include "gpio.h"
+
+#include "interrupt.h"
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      GPIO 管脚复用配置
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+static void GPIOBankPinMuxSet()
+{
+    // 配置相应的 GPIO 口功能为普通输入输出口
+    // 底板 LED
+    // GPIO2[15]
+    HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(05)) = (HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(05)) & (~(SYSCFG_PINMUX5_PINMUX5_3_0))) |
+                                                    ((SYSCFG_PINMUX5_PINMUX5_3_0_GPIO2_15 << SYSCFG_PINMUX5_PINMUX5_3_0_SHIFT));
+
+    // GPIO4[00]
+    HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(10)) = (HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(10)) & (~(SYSCFG_PINMUX10_PINMUX10_31_28))) |
+                                                    ((SYSCFG_PINMUX10_PINMUX10_31_28_GPIO4_0 << SYSCFG_PINMUX10_PINMUX10_31_28_SHIFT));
+
+    // 按键
+    // GPIO0[8]
+    HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(00)) = (HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(00)) & (~(SYSCFG_PINMUX0_PINMUX0_31_28))) |
+                                                    ((SYSCFG_PINMUX0_PINMUX0_31_28_GPIO0_8 << SYSCFG_PINMUX0_PINMUX0_31_28_SHIFT));
+
+    // GPIO8[12]
+    HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(18)) = (HWREG(SOC_SYSCFG_0_REGS + SYSCFG0_PINMUX(18)) & (~(SYSCFG_PINMUX18_PINMUX18_23_20))) |
+                                                    ((SYSCFG_PINMUX18_PINMUX18_23_20_GPIO8_12 << SYSCFG_PINMUX18_PINMUX18_23_20_SHIFT));
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      GPIO 管脚初始化
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+static void GPIOBankPinInit()
+{
+    // 底板 LED
+    GPIODirModeSet(SOC_GPIO_0_REGS, 48, GPIO_DIR_OUTPUT);   // GPIO2[15] LED4
+    GPIODirModeSet(SOC_GPIO_0_REGS, 65, GPIO_DIR_OUTPUT);   // GPIO4[00] LED3
+
+    // 按键
+    // 配置中断触发方式
+    GPIOIntTypeSet(SOC_GPIO_0_REGS, 9, GPIO_INT_TYPE_FALLEDGE);         // SW4 下降沿
+    GPIOIntTypeSet(SOC_GPIO_0_REGS, 141, GPIO_INT_TYPE_FALLEDGE);       // SW6 上升沿及下降沿
+
+    // 使能 GPIO BANK 中断
+    GPIOBankIntEnable(SOC_GPIO_0_REGS, 0);                              // GPIO BANK0
+    GPIOBankIntEnable(SOC_GPIO_0_REGS, 8);                              // GPIO BANK8
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      延时（非精确）
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+static void Delay(volatile unsigned int delay)
+{
+    while(delay--);
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      中断服务函数
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// StarterWare system_config.lib 驱动库内部已使用 interrupt 关键字修饰 此处仅为回调函数
+void KEY4Isr()
+{
+    if(GPIOPinIntStatus(SOC_GPIO_0_REGS, 141) == GPIO_INT_PEND)
+    {
+        GPIOPinWrite(SOC_GPIO_0_REGS, 65, GPIO_PIN_HIGH);
+        Delay(0x00FFFFFF);  // 此处仅为演示方便 请勿在中断服务函数使用类似语句
+
+        GPIOPinWrite(SOC_GPIO_0_REGS, 65, GPIO_PIN_LOW);
+        Delay(0x00FFFFFF);
+    }
+
+    // 清除 GPIO 中断状态
+    GPIOPinIntClear(SOC_GPIO_0_REGS, 141);
+}
+
+void KEY6Isr()
+{
+    if(GPIOPinIntStatus(SOC_GPIO_0_REGS, 9) == GPIO_INT_PEND)
+    {
+        // 闪烁 LED
+        GPIOPinWrite(SOC_GPIO_0_REGS, 48, GPIO_PIN_HIGH);
+        Delay(0x00FFFFFF);  // 此处仅为演示方便 请勿在中断服务函数使用类似语句
+
+        GPIOPinWrite(SOC_GPIO_0_REGS, 48, GPIO_PIN_LOW);
+        Delay(0x00FFFFFF);
+    }
+
+    // 清除 GPIO 中断状态
+    GPIOPinIntClear(SOC_GPIO_0_REGS, 9);
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      DSP 中断初始化
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+void InterruptInit(void)
+{
+    // 初始化 DSP 中断控制器
+    IntDSPINTCInit();
+
+    // 使能 DSP 全局中断
+    IntGlobalEnable();
+
+    // 注册中断服务函数
+    IntRegister(C674X_MASK_INT4, KEY4Isr);
+    IntRegister(C674X_MASK_INT5, KEY6Isr);
+
+    // 映射中断到 DSP 可屏蔽中断
+    IntEventMap(C674X_MASK_INT4, SYS_INT_GPIO_B8INT);
+    IntEventMap(C674X_MASK_INT5, SYS_INT_GPIO_B0INT);
+
+    // 使能 DSP 可屏蔽中断
+    IntEnable(C674X_MASK_INT4);
+    IntEnable(C674X_MASK_INT5);
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+//      主函数
+//
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+void main()
+{
+    // 使能外设
+    PSCModuleControl(SOC_PSC_1_REGS, HW_PSC_GPIO, PSC_POWERDOMAIN_ALWAYS_ON, PSC_MDCTL_NEXT_ENABLE);
+
+    // 管脚复用配置
+    GPIOBankPinMuxSet();
+
+    // GPIO 管脚初始化
+    GPIOBankPinInit();
+
+    // DSP 中断初始化
+    InterruptInit();
+
+    for(;;)
+    {
+
+    }
+}
